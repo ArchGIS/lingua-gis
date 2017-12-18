a86737a57eee581bbbc9202316056e1c344ed4e0
Делюс Фархуллин <bontequero@gmail.com>
2017-12-18 13:46:06 +0300

    change cluster radius

--- fs/web_client/js/views/map.js
+++ fs/web_client/js/views/map.js
@@ -120,7 +120,7 @@ App.views.createOverlays = (leaf, types) => {
   const rmax = 30;
 
   const cluster = L.markerClusterGroup({
-    maxClusterRadius: 10,
+    maxClusterRadius: 0,
     iconCreateFunction: (cluster) => {
       const markers = cluster.getAllChildMarkers(),
             n = markers.length,
